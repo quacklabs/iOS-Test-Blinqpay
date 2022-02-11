50278b1374602b20bb596ff676ad139fe2faa18d
Mark Boleigha <donhoenix@gmail.com>
2022-02-11 10:48:03 +0100

    Initial Commit

---
+++ iOS-Test-BlinqpayTests/iOS_Test_BlinqpayTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Test_BlinqpayTests.swift
+//  iOS-Test-BlinqpayTests
+//
+//  Created by Mark Boleigha on 11/02/2022.
+//  Copyright © 2022 Blinqpay. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Test_Blinqpay
+
+class iOS_Test_BlinqpayTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
